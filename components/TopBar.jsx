7729def7b494c069c745a0586a27353ea25104f2
rush1dan <rushidan10@gmail.com>
2023-11-20 09:21:05 +0600

    Logout functionality added

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -2,19 +2,32 @@
 
 import PageContext from '@/state/PageContext';
 import React, { useContext } from 'react'
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const TopBar = (props) => {
     const {selectedPage, setSelectedPage} = useContext(PageContext);
 
+    const router = useRouter();
+    async function handleSignOut() {
+        await signOut({ redirect: false });
+        router.push('/login');
+    }
+
     return (
         <div className='w-full h-16 px-4 bg-blue-400'>
-            <div className='w-full h-full flex flex-row items-center justify-start gap-x-6'>
+            <div className='w-full h-full flex flex-row items-center justify-start gap-x-12 bg-red-200'>
                 <p>Finealth</p>
-                <button onClick={() => setSelectedPage('Monthly')} className={selectedPage === 'Monthly' ? 'font-bold' : ''}>
-                    Monthly
-                </button>
-                <button onClick={() => setSelectedPage('Yearly')} className={selectedPage === 'Yearly' ? 'font-bold' : ''}>
-                    Yearly
+                <div className='flex flex-row items-center justify-center gap-x-6'>
+                    <button onClick={() => setSelectedPage('Monthly')} className={selectedPage === 'Monthly' ? 'font-bold' : ''}>
+                        Monthly
+                    </button>
+                    <button onClick={() => setSelectedPage('Yearly')} className={selectedPage === 'Yearly' ? 'font-bold' : ''}>
+                        Yearly
+                    </button>
+                </div>
+                <button onClick={() => handleSignOut()} className='mr-0'>
+                    Logout
                 </button>
             </div>
         </div>
