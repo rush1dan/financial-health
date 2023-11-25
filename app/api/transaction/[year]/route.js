3f3ecff05516067ccb0726065c899d21fb347cb8
rush1dan <rushidan10@gmail.com>
2023-11-25 17:48:12 +0600

    Year and month validation

--- app/api/transaction/[year]/route.js
+++ app/api/transaction/[year]/route.js
@@ -5,13 +5,17 @@ import { getToken } from "next-auth/jwt";
 export const dynamic = 'force-dynamic' // defaults to force-static
 export async function GET(req, {params}) {
     try {
+        console.log("TYPE: ", typeof params.year);
+        if (isNaN(params.year)) {
+            throw new Error("Param type must be integer");
+        }
         const secret = process.env.NEXTAUTH_SECRET
         const token = await getToken({ req, secret });
 
         const userId = token.id;
 
         await connectToMongoDB();
-        const transactions = params.year ? await getYearlyTransactions(userId, params.year) : [];
+        const transactions = await getYearlyTransactions(userId, params.year);
 
         console.log("Transaction Retrieved Successfully");
         return new NextResponse(JSON.stringify(transactions), { status: 200 });
